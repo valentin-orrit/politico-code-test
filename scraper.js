78cb155a4b7d158eabf70205173856736049e456
valentin-orrit <vorrit@gmail.com>
2025-04-22 16:25:31 +0200

    added name formatting helper function + parsing of lastName, partyGroup, country and url

--- scraper.js
+++ scraper.js
@@ -23,8 +23,19 @@ export const scrapeEULegislators = async () => {
       const legislatorsData = $('.erpl_member-list-item')
         .map((i, el) => {
           const name = $(el).find('.erpl_title-h4').text()
+          const lastName = name.split(' ').slice(-1)[0]
+          const additionalInfos = $(el).find('.sln-additional-info')
+          const partyGroup = $(additionalInfos[0]).text()
+          const country = $(additionalInfos[1]).text()
 
-          return { name }
+          // reconstructing Full URL with formatted name
+          const parsedUrl = $(el)
+            .find('.erpl_member-list-item-content')
+            .attr('href')
+          const formattedName = formatLegislatorsName(name)
+          const url = `${parsedUrl}/${formattedName}/home`
+
+          return { name, lastName, partyGroup, country, url }
         })
         .get()
 
@@ -38,3 +49,13 @@ export const scrapeEULegislators = async () => {
 }
 
 scrapeEULegislators()
+
+// Helper function to format LegislatorsName to working URL
+const formatLegislatorsName = (name) => {
+  return name
+    .toUpperCase()
+    .replace(/\s+/g, '_') // replaces spaces with underscores
+    .normalize('NFD') // Decomposes accented letters to letter + accent
+    .replace(/[\u0300-\u036f]/g, '') // Removes accents
+    .replace(/[^A-Z_]/g, '') // Replace other non latin letters + punctuation
+}
