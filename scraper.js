f68fa1581b57e98dbd0a5a6a44c1fde3b6e98917
valentin-orrit <vorrit@gmail.com>
2025-04-22 15:31:32 +0200

    added function to parse the legislators data

--- scraper.js
+++ scraper.js
@@ -2,17 +2,39 @@ import * as cheerio from 'cheerio'
 import axios from 'axios'
 
 export const scrapeEULegislators = async () => {
-  // fetching the legislators data
+  // Fetching the legislators data
   const loadLegistlators = async () => {
     try {
       const page = await axios.get(
         'https://www.europarl.europa.eu/meps/en/full-list/all'
       )
-      return page
+      return page.data
     } catch (error) {
       console.error('error when fetching or loading the page : ', error)
     }
   }
 
-  return []
+  // Parsing the legislators data
+  const parseLegislatorsData = async () => {
+    try {
+      const html = await loadLegistlators()
+      const $ = cheerio.load(html)
+
+      const legislatorsData = $('.erpl_member-list-item')
+        .map((i, el) => {
+          const name = $(el).find('.erpl_title-h4').text()
+
+          return { name }
+        })
+        .get()
+
+      return legislatorsData
+    } catch (error) {
+      console.error('error when parsing the data : ', error)
+    }
+  }
+
+  return await parseLegislatorsData()
 }
+
+scrapeEULegislators()
