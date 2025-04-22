76f87112203e08ab082c85d7a2ef3a25a860fa7a
valentin-orrit <vorrit@gmail.com>
2025-04-22 15:01:52 +0200

    added function to load the legislators data

--- scraper.js
+++ scraper.js
@@ -1,5 +1,18 @@
+import * as cheerio from 'cheerio'
+import axios from 'axios'
+
 export const scrapeEULegislators = async () => {
-  // @TODO: implement a scraper for MEPs
+  // fetching the legislators data
+  const loadLegistlators = async () => {
+    try {
+      const page = await axios.get(
+        'https://www.europarl.europa.eu/meps/en/full-list/all'
+      )
+      return page
+    } catch (error) {
+      console.error('error when fetching or loading the page : ', error)
+    }
+  }
 
   return []
 }
